ac7d32b071d81c95468a2f14684aff984cf51d7b
simiadi <simonadam006@gmail.com>
2024-10-08 14:22:00 +0200

    fix

--- hazifeladatok/regex/numberLength2Or3/script.js
+++ hazifeladatok/regex/numberLength2Or3/script.js
@@ -1,7 +1,7 @@
 let text = "Hello WORLD! Today is the 5th of June 2024 21th century. I've 15 apples, 123 oranges, and 4567 grapes."
 let regex, result
 
-regex = /\b\d{2}\b/g
+regex = /\b\d{2,3}\b/g
 result = text.match(regex)
 
 console.log(`\nOlyan számok keresése, melyek 2 vagy 3 számjegyet tartalmaznak: ${regex}`)
